ad2dbde179b9310b9cf17b3caf7b6c893a718396
Jagadish Jena <jenasuraj218@gmail.com>
2026-09-15 02:44:22 +0530

    Update Vercel and server

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,12 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-
 
 app.get("/api/health", (req, res) => {
   res.json({ message: "API running" });
 });
-
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/transactions", require("./routes/transactionRoutes"));
